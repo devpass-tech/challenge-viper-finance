df63a9513108f974792e928602dc4edd2e415d38
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2022-02-02 09:49:26 -0300

    module creation in the VIPER structure

---
+++ solutions/devsprint-julio-fernandes-1/FinanceApp/Modules/ContactList/ContactListProtocols.swift
@@ -0,0 +1,26 @@
+//
+//  ContactListProtocols.swift
+//  FinanceApp
+//
+//  Created by Alexandre Cardoso on 02/02/22.
+//
+
+import UIKit
+
+protocol ContactListPresenterProtocol {
+	var view: ContactListPresenterDelegate? { get set }
+	var interactor: ContactListInteractorProtocol { get set }
+	var router: ContactListRouterProtocol { get set }
+	
+	func viewDidLoad()
+}
+
+protocol ContactListRouterProtocol {
+	static func createModule() -> UINavigationController
+	func navigateToNewModule()
+}
+
+protocol ContactListInteractorProtocol {
+	var presenter: ContactListInteractorDelegate? { get set }
+	func fetchData()
+}
