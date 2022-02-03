a6f8bc2f7a3d886b1a1fbfe9171068a22bbae879
Lucas Eduardo Schlögl <leschlogl@users.noreply.github.com>
2022-02-02 23:41:54 -0300

    improved confirmation router to inject dependencies

--- solutions/devsprint-julio-fernandes-1/FinanceApp/Modules/Confirmation/ConfirmationViewController.swift
+++ solutions/devsprint-julio-fernandes-1/FinanceApp/Modules/Confirmation/ConfirmationViewController.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 final class ConfirmationViewController: UIViewController {
-    var presenter: ConfirmationPresenterProtocol?
+    var presenter: ConfirmationPresenterProtocol
+
+    init(presenter: ConfirmationPresenterProtocol) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available (*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        presenter?.viewDidLoad()
+        presenter.viewDidLoad()
     }
 
     override func loadView() {
