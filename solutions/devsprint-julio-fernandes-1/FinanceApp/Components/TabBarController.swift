c0cb7346e0f447b400472ef9739f32f7b471810d
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2022-02-07 19:52:44 -0300

    fix code

--- solutions/devsprint-julio-fernandes-1/FinanceApp/Components/TabBarController.swift
+++ solutions/devsprint-julio-fernandes-1/FinanceApp/Components/TabBarController.swift
@@ -16,7 +16,7 @@ class TabBarController: UITabBarController {
     override func viewWillAppear(_ animated: Bool) {
 
         let homeViewController = HomeRouter.createModule()
-let homeNavigationController = UINavigationController(rootViewController: homeViewController)
+		 let homeNavigationController = UINavigationController(rootViewController: homeViewController)
         let homeTabBar = UITabBarItem(title: "Home", image: UIImage(named: "house.fill"), tag: 0)
         homeNavigationController.tabBarItem = homeTabBar
 
