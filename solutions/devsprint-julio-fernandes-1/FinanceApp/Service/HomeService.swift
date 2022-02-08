d3ea380bcd851c5ffec02c594a762756e059aea9
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2022-02-08 11:40:50 -0300

    add layer service

--- solutions/devsprint-julio-fernandes-1/FinanceApp/Service/HomeService.swift
+++ solutions/devsprint-julio-fernandes-1/FinanceApp/Service/HomeService.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+protocol HomeService {
+	func load(urlString: String, completion: @escaping (Result<HomeEntity, NSError>) -> Void)
+}
+
+struct HomeAPI: HomeService {
+	
+	func load(urlString: String, completion: @escaping (Result<HomeEntity, NSError>) -> Void) {
+		guard let url = URL(string: urlString) else {
+			return completion(.failure(NSError(domain: "Error URL", code: 0)))
+		}
+		
+		let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+			if let error = error {
+				completion(.failure(NSError(domain: error.localizedDescription, code: 0)))
+			}
+			
+			if let response = response as? HTTPURLResponse,
+				!((200...299).contains(response.statusCode)) {
+				completion(.failure(NSError(domain: "Error Http status-code", code: response.statusCode)))
+			}
+			
+			guard let data = data else {
+				return completion(.failure(NSError(domain: "No Data", code: 0)))
+			}
+			
+			do {
+				let result = try JSONDecoder().decode(HomeEntity.self, from: data)
+				completion(.success(result))
+			} catch let error {
+				completion(.failure(NSError(domain: error.localizedDescription, code: 0)))
+			}
+		}
+		
+		dataTask.resume()
+	}
+	
+}
