e2bb8c325a1b6e4a11d071d53d236074ade3c4e1
Mateus N. Coelho <mateusnazarioc@gmail.com>
2022-02-09 14:29:42 -0300

    adding home router

---
+++ solutions/devsprint-julio-fernandes-1/FinanceApp/Modules/Home/HomeRouter.swift
@@ -0,0 +1,30 @@
+//
+//  HomeRouter.swift
+//  FinanceApp
+//
+//  Created by Mateus Nazario on 09/02/22.
+//
+
+import UIKit
+
+typealias HomeInterable = HomePresenterProtocol & HomeInteractorDelegate
+
+final class HomeRouter: HomeRouterProtocol {
+    static func createModule() -> UIViewController {
+        let viewController: HomeViewController = HomeViewController()
+        let interactor = HomeInteractor()
+        let router = HomeRouter()
+        let presenter: HomeInterable = HomePresenter(
+            interactor: interactor,
+            router: router
+        )
+        
+        viewController.presenter = presenter
+        viewController.presenter?.router = HomeRouter()
+        viewController.presenter?.view = viewController
+        viewController.presenter?.interactor = HomeInteractor()
+        viewController.presenter?.interactor?.presenter = presenter
+        
+        return viewController
+    }
+}
