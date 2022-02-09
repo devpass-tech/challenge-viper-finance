85785534eed8058ed7bc236a88eaf50fc24fcfd1
Mateus N. Coelho <mateusnazarioc@gmail.com>
2022-02-09 14:30:00 -0300

    changing home view and viewController to support VIPER

--- solutions/devsprint-julio-fernandes-1/FinanceApp/Modules/Home/HomeViewController.swift
+++ solutions/devsprint-julio-fernandes-1/FinanceApp/Modules/Home/HomeViewController.swift
@@ -8,36 +8,45 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    // MARK: Properties
+    var presenter: HomePresenterProtocol?
+    
+    // MARK: Components
     lazy var homeView: HomeView = {
-
         let homeView = HomeView()
         homeView.delegate = self
         return homeView
     }()
-
+    
+    // MARK: Init
     override func viewDidLoad() {
-
+        presenter?.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Profile", style: .plain, target: self, action: #selector(openProfile))
     }
 
     override func loadView() {
         self.view = homeView
     }
-
+    
+    // MARK: Methods
     @objc
     func openProfile() {
-		  let navigationController = UserProfileRouter.createModule()
+        let navigationController = UserProfileRouter.createModule()
         self.present(navigationController, animated: true)
     }
 }
 
+// MARK: Extensions
 extension HomeViewController: HomeViewDelegate {
-
     func didSelectActivity() {
-        
         let activityDetailsViewController = ActivityDetailsRouter.createModule()
-        
         self.navigationController?.pushViewController(activityDetailsViewController, animated: true)
+    }
+}
+
+extension HomeViewController: HomePresenterDelegate {
+    func showData() {
+        print("HomeViewController - HomePresenterDelegate - showData()")
     }
 }
