6945c204350d60333a2ad97a624012709aa52570
Victor Catao <victorcatao@hotmail.com>
2022-03-21 16:35:26 -0300

    Add some marks

--- solutions/devsprint-bruno-faganello-1/FinanceApp/Service/Home/HomeService.swift
+++ solutions/devsprint-bruno-faganello-1/FinanceApp/Service/Home/HomeService.swift
@@ -5,11 +5,18 @@
 //  Created by Victor Catão on 20/03/22.
 //
 
+// MARK: - HomeServiceProtocol
+
 protocol HomeServiceProtocol {
     func fetchHomeInformation() async -> Result<HomeData, RequestError>
 }
 
+// MARK: - HomeService
+
 struct HomeService: HTTPClient, HomeServiceProtocol {
+    
+    // MARK: Public Methods
+    
     func fetchHomeInformation() async -> Result<HomeData, RequestError> {
         return await sendRequest(endpoint: HomeEndpoint.homeInformation, responseModel: HomeData.self)
     }
--- solutions/devsprint-bruno-faganello-1/FinanceApp/Service/Repository/HomeRepository.swift
+++ solutions/devsprint-bruno-faganello-1/FinanceApp/Service/Repository/HomeRepository.swift
@@ -7,22 +7,34 @@
 
 import Foundation
 
+// MARK: - HomeError
+
 enum HomeError {
     case unknown
 }
+
+// MARK: - HomeRepositoryProtocol
 
 protocol HomeRepositoryProtocol {
     func fetchHomeData(completion: @escaping (HomeData?, HomeError?) -> Void)
 }
 
+// MARK:  - HomeRepository
+
 struct HomeRepository: HomeRepositoryProtocol {
     
+    // MARK: Private Properties
+
     private let service: HomeServiceProtocol
     
+    // MARK: Init
+
     init(service: HomeServiceProtocol = HomeService()) {
         self.service = service
     }
     
+    // MARK: Public Methods
+
     func fetchHomeData(completion: @escaping (HomeData?, HomeError?) -> Void) {
         Task.init(priority: .background) {
             let result = await service.fetchHomeInformation()
