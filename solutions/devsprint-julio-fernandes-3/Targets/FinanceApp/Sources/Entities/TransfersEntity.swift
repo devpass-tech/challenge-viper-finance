ea4f89cfd8f37e27fdc413d4738e6e88062cd737
Pedro Henrique Martins Barbosa <pbarbosa@ciandt.com>
2022-05-29 15:11:10 -0300

    Add transfers entity

---
+++ solutions/devsprint-julio-fernandes-3/Targets/FinanceApp/Sources/Entities/TransfersEntity.swift
@@ -0,0 +1,13 @@
+//
+//  TransfersEntity.swift
+//  FinanceApp
+//
+//  Created by Pedro Henrique Martins Barbosa on 29/05/22.
+//  Copyright © 2022 tuist.io. All rights reserved.
+//
+
+import Foundation
+
+struct TransfersEntity: Codable {
+    let success: Bool
+}
