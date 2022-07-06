37be7348289b38df35cc16fdcdf84b7fb1786c5d
Caio Santos <caio.santos@xpi.com.br>
2022-07-05 22:17:53 -0300

    Migrating UserProfile module.

--- solutions/devsprint-caio-santos-6/FinanceApp/Modules/UserProfile/UserProfileViewController.swift
+++ solutions/devsprint-caio-santos-6/FinanceApp/Modules/UserProfile/UserProfileViewController.swift
@@ -9,7 +9,80 @@ import UIKit
 
 class UserProfileViewController: UIViewController {
 
-    override func loadView() {
-        self.view = UserProfileView()
+    private lazy var tableView: UITableView = {
+
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = self
+
+        let headerView = UserProfileHeaderView()
+        headerView.frame = CGRect(x: 0, y: 0, width: 0, height: 232)
+        tableView.tableHeaderView = headerView
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        setupView()
+        // presenter.
     }
+}
+
+// MARK: - Private Methods
+
+extension UserProfileViewController {
+    private func setupView() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension UserProfileViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "Cell")
+
+        switch indexPath.row {
+        case 0:
+
+            cell.textLabel?.text = "Phone"
+            cell.detailTextLabel?.text = "+55 (11) 99999-9999"
+        case 1:
+
+            cell.textLabel?.text = "E-mail"
+            cell.detailTextLabel?.text = "user@devpass.com"
+        case 2:
+
+            cell.textLabel?.text = "Address"
+            cell.detailTextLabel?.text = "Rua Bela Cintra, 495"
+        default:
+            break
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "My account"
+    }
+
 }
