9792d06149f528fa7e463e5e18e4476bc292cb64
waldisonjunior <waldison.junior@popcode.com.br>
2022-11-16 20:33:24 -0300

    feature: creates the interactor layer and implements the same

---
+++ solutions/devsprint-pedro-alvarez-1/FinanceApp/Modules/ActivityDetails/Business/ActivityDetailsInteractor.swift
@@ -0,0 +1,30 @@
+//
+//  ActivityDetailsInteractor.swift
+//  FinanceApp
+//
+//  Created by Junior Fernandes on 16/11/22.
+//
+
+protocol ActivityDetailsInteractorInput {
+    func fetchDetails()
+}
+
+protocol ActivityDetailsInteractorOutput: AnyObject {
+    func didFetchDetail(_ data: ActivityDetails)
+}
+
+final class ActivityDetailsInteractor: ActivityDetailsInteractorInput {
+    private let service: FinanceServiceProtocol
+    weak var  presenter: ActivityDetailsInteractorOutput?
+    
+    init(service: FinanceServiceProtocol) {
+        self.service = service
+    }
+    
+    func fetchDetails() {
+        service.fetchActivityDetails { [weak self] result in
+            guard let data = result else { return }
+            self?.presenter?.didFetchDetail(data)
+        }
+    }
+}
